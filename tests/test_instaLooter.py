9726ca144ad793f414ca96e7121133e5c7eca349
Martin Larralde <martin.larralde@ens-cachan.fr>
2016-12-15 15:02:21 +0000

    Remove unexisting profiles from test list

--- tests/test_instaLooter.py
+++ tests/test_instaLooter.py
@@ -14,14 +14,14 @@ import instaLooter
 
 
 
-class TestInstaLooterDownload(unittest.TestCase):
+class TestInstaLooterProfileDownload(unittest.TestCase):
 
     MOST_POPULAR = [
         'instagram', 'selenagomez', 'taylorswift',
         'arianagrande', 'beyonce', 'kimkardashian',
-        'cristiano', 'kyliejenner', 'justinbieber',
-        'therock'
+        'cristiano', 'kyliejenner', 'therock',
     ]
+
 
     def setUp(self):
         self.tmpdir = tempfile.mkdtemp()
@@ -33,14 +33,18 @@ class TestInstaLooterDownload(unittest.TestCase):
     def register_tests(cls):
 
         for profile in cls.MOST_POPULAR:
+            cls._register_test(profile)
 
-            def _test(self):
-                looter = instaLooter.InstaLooter(self.tmpdir, profile=profile, get_videos=True)
-                looter.download()
-                self.assertEqual(len(os.listdir(self.tmpdir)), int(looter.metadata['media']['count']))
-                self.assertEqual(profile, looter.metadata['username'])
+    @classmethod
+    def _register_test(cls, profile):
 
-            setattr(cls, "test_{}".format(profile), _test)
+        def _test(self):
+            looter = instaLooter.InstaLooter(self.tmpdir, profile=profile, get_videos=True)
+            looter.download(media_count=200)
+            self.assertEqual(len(os.listdir(self.tmpdir)), min(200, int(looter.metadata['media']['count'])))
+            self.assertEqual(profile, looter.metadata['username'])
+
+        setattr(cls, "test_{}".format(profile), _test)
 
 
 class TestInstaLooterHashtagDownload(unittest.TestCase):
@@ -62,8 +66,8 @@ class TestInstaLooterHashtagDownload(unittest.TestCase):
 
 def load_tests(loader, tests, pattern):
     suite = unittest.TestSuite()
-    TestInstaLooterDownload.register_tests()
-    suite.addTests(loader.loadTestsFromTestCase(TestInstaLooterDownload))
+    TestInstaLooterProfileDownload.register_tests()
+    suite.addTests(loader.loadTestsFromTestCase(TestInstaLooterProfileDownload))
     suite.addTests(loader.loadTestsFromTestCase(TestInstaLooterHashtagDownload))
     return suite
 
