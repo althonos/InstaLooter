239306a836df44eacf33a609fc3f285d3d752792
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-04-17 22:20:18 +0200

    Stop skipping hashtag download tests since feature works back again

--- tests/test_instaLooter.py
+++ tests/test_instaLooter.py
@@ -54,10 +54,10 @@ class TestInstaLooterHashtagDownload(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.tmpdir)
 
-    # def test_hashtag_download(self):
-    #     looter = instaLooter.InstaLooter(self.tmpdir, hashtag="python", get_videos=True)
-    #     looter.download(media_count=200)
-    #     self.assertEqual(len(os.listdir(self.tmpdir)), 200)
+    def test_hashtag_download(self):
+        looter = instaLooter.InstaLooter(self.tmpdir, hashtag="python", get_videos=True)
+        looter.download(media_count=200)
+        self.assertEqual(len(os.listdir(self.tmpdir)), 200)
 
 
 class TestInstaLooterTemplate(unittest.TestCase):
