1d49dbdb347124a662073f9847e263cf7f6e1d56
AndCycle <andcycle-git@andcycle.idv.tw>
2017-09-11 20:10:22 +0800

    Add test for save metadata

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import tempfile
 import unittest
+import json
 
 import instaLooter
 
@@ -26,6 +27,19 @@ class TestInstaLooterCommandLineInterface(unittest.TestCase):
 
         instaLooter.main(["post", "BIqZ8L8AHmH", self.tmpdir])
         self.assertIn("1308972728853756295.jpg", os.listdir(self.tmpdir))
+
+    def test_cli_single_save_metadata(self):
+        instaLooter.main(
+            ["post", "https://www.instagram.com/p/BFB6znLg5s1/", self.tmpdir, "--save-metadata"]
+        )
+
+        instaLooter.main(["post", "BIqZ8L8AHmH", self.tmpdir])
+        self.assertIn("1243533605591030581.jpg.json", os.listdir(self.tmpdir))
+
+        with open(os.path.join(self.tmpdir, "1243533605591030581.jpg.json")) as fp:
+            json_metadata = json.load(fp)
+        self.assertEqual("1243533605591030581", json_metadata["id"])
+        self.assertEqual("BFB6znLg5s1", json_metadata["code"])
 
     def test_cli_no_directory_fail(self):
         self.assertTrue(
