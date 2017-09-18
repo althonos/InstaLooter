a7442d509b4bcbc25da071eb82f3117015ea3415
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-09-18 14:24:36 +0200

    Release v0.13.2

--- instaLooter/__init__.py
+++ instaLooter/__init__.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 __author__ = "althonos"
 __author_email__ = "martin.larralde@ens-cachan.fr"
-__version__ = "0.13.1"
+__version__ = "0.13.2"
 __license___ = "GPL-3+"
 
 try:
