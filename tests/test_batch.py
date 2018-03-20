a720a68850c76a7a6f9fc0d11081f0a8c8b3e918
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-03-20 01:41:17 +0100

    Add tests for `BatchRunner`

---
+++ tests/test_batch.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import textwrap
+import unittest
+import warnings
+
+import fs
+
+from instaLooter.cli import main
+from instaLooter.batch import BatchRunner
+
+
+
+class TestBatchRunner(unittest.TestCase):
+
+    def setUp(self):
+        self.destfs = fs.open_fs("temp://")
+        self.tmpdir = self.destfs.getsyspath("/")
+
+    def tearDown(self):
+        self.destfs.close()
+
+    def test_cli(self):
+
+        cfg = textwrap.dedent(
+            """
+            [my job]
+
+            num-to-dl = 3
+            quiet = true
+
+            users:
+                therock: {self.tmpdir}
+                nintendo: {self.tmpdir}
+            """
+        ).format(self=self)
+
+        with self.destfs.open('batch.ini', 'w') as batch_file:
+            batch_file.write(cfg)
+
+        retcode = main(["batch", self.destfs.getsyspath('batch.ini')])
+        self.assertEqual(retcode, 0)
+        self.assertEqual(len(list(self.destfs.filterdir("/", ["*.jpg"]))), 6)
+
+
+
+def setUpModule():
+   warnings.simplefilter('ignore')
+
+
+def tearDownModule():
+   warnings.simplefilter(warnings.defaultaction)
