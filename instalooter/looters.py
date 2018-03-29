4ca3095b5d824e5f19428e55c02f41c8e1775f39
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2018-03-29 08:04:27 +0200

    Second attempt at fixing Codacy security issue with pseudo-random timer.

--- instalooter/looters.py
+++ instalooter/looters.py
@@ -117,7 +117,7 @@ class InstaLooter(object):
 
         with session.get(homepage) as res:
             session.headers.update({'X-CSRFToken': res.cookies['csrftoken']})
-            time.sleep(5 * random.random())
+        time.sleep(5 * random.random()) # nosec
 
         with session.post(login_url, data=data, allow_redirects=True) as login:
             session.headers.update({'X-CSRFToken': login.cookies['csrftoken']})
