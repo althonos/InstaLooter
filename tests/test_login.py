c066d41ed09881dfb07ee972a196251d48ac83e2
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-03-30 15:50:12 +0200

    Add tests to check login feature

---
+++ tests/test_login.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import os
+import unittest
+
+from instalooter.looters import ProfileLooter
+
+
+USERNAME = os.getenv("IG_USERNAME")
+PASSWORD = os.getenv("IG_PASSWORD")
+
+
+class TestLogin(unittest.TestCase):
+
+    @unittest.skipUnless(USERNAME and PASSWORD, "credentials required")
+    def test_login(self):
+
+        looter = ProfileLooter(USERNAME)
+
+        self.assertFalse(looter.logged_in())
+        self.assertRaises(StopIteration, lambda: next(looter.medias()))
+        self.assertFalse(looter.cachefs.exists(looter._COOKIE_FILE))
+
+        try:
+            looter.login(USERNAME, PASSWORD)
+            self.assertTrue(looter.logged_in())
+            self.assertTrue(looter.cachefs.exists(looter._COOKIE_FILE))
+            self.assertTrue(next(looter.medias()))
+        finally:
+            looter.logout()
+            self.assertFalse(looter.cachefs.exists(looter._COOKIE_FILE))
