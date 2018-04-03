12e3de1e4bdab77546e4b877cfdc65397cb15647
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-04-03 14:28:00 +0200

    Release v1.0.0-a.4
    Signed-off-by: Martin Larralde <martin.larralde@ens-cachan.fr>

--- instalooter/__init__.py
+++ instalooter/__init__.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 
 __author__ = "Martin Larralde"
 __author_email__ = "martin.larralde@ens-cachan.fr"
-__version__ = "1.0.0-a.3"
+__version__ = "1.0.0-a.4"
 __license___ = "GPLv3+"
